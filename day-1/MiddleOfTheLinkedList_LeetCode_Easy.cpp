0a6ce61e6a4d5617ca1b5666167c53c4de56686b
DEENDAYAL <vdeendayal866@gmail.com>
2026-04-23 23:58:34 +0530

    ✅ Day-1: Middle of the Linked List [LeetCode] — Easy

---
+++ day-1/MiddleOfTheLinkedList_LeetCode_Easy.cpp
@@ -0,0 +1,34 @@
+// ═══════════════════════════════════════════════════
+//  DSA Tracker — Auto-pushed by Extension
+// ═══════════════════════════════════════════════════
+//  Question  : Middle of the Linked List
+//  URL       : https://leetcode.com/problems/middle-of-the-linked-list/
+//  Platform  : LeetCode
+//  Difficulty: Easy
+//  Language  : cpp
+//  Day       : Day-1
+//  Date      : 2026-04-23
+// ═══════════════════════════════════════════════════
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode*fast = head;
+        ListNode*slow = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
